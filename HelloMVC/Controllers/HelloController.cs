b806c79baa059e98cb987279ef4bb49c73fb727e
Brian <bdburns6389@gmail.com>
2018-01-21 21:35:14 -0600

    Added first input method

---
+++ HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post'> " +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        // /Hello
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        // Handle requests to /Hello/NAME (URL Segment)
+
+        // /Hello/Goodbye
+        // Alter the route to this controller to be:  /Hello/Aloha
+        public IActionResult Goodbye()
+        {
+            return Content("GoodBye");
+        }
+    }
+}
